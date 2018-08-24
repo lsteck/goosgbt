e07e89263f68c1064cda1f6a1bf92129181615ec
WIL PANNELL <wgpannel@us.ibm.com>
2018-08-24 03:11:05 -0500

    resolve reference to driver.showSniperStatus

--- src/test/java/com/larry/steck/goos/auction/drivers/AuctionSniperDriver.java
+++ src/test/java/com/larry/steck/goos/auction/drivers/AuctionSniperDriver.java
@@ -3,4 +3,7 @@ package com.larry.steck.goos.auction.drivers;
 public class AuctionSniperDriver {
   public AuctionSniperDriver(int timeoutMilliseconds) {
   }
+
+  public void showsSniperStatus(String statusJoining) {
+  }
 }
