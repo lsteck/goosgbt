e1a6e8ab4e4cb301329e7e42fefa5be55ac39e8c
WIL PANNELL <wgpannel@us.ibm.com>
2018-08-25 06:54:26 -0500

    receive next failing test message

--- src/main/java/com/larry/steck/goos/auction/MainWindow.java
+++ src/main/java/com/larry/steck/goos/auction/MainWindow.java
@@ -1,15 +1,29 @@
 package com.larry.steck.goos.auction;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 import static com.larry.steck.goos.auction.Main.MAIN_WINDOW_NAME;
 
 public class MainWindow extends JFrame {
+  public static final String SNIPER_STATUS_NAME = "sniper status";
+  public static final String STATUS_JOINING = "Joining";
+
+  private final JLabel sniperStatus = createLabel(STATUS_JOINING);
   public MainWindow() throws HeadlessException {
     super("Auction Sniper");
     setName(MAIN_WINDOW_NAME);
+    add(sniperStatus);
+    pack();
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
+  }
+
+  private static JLabel createLabel(String initialText) {
+    JLabel result = new JLabel(initialText);
+    result.setName(SNIPER_STATUS_NAME);
+    result.setBorder(new LineBorder(Color.BLACK));
+    return result;
   }
 }
